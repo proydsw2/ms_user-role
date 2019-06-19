1ac8d5936535a5ee0ee1b7f2322273e4afe92e70
VFConsulting <jorge.larosa@vfcons.com>
2019-06-19 15:08:50 -0500

    port 8081

--- src/main/java/com/cibertec/ms_userrole/Controller/UserRoleController.java
+++ src/main/java/com/cibertec/ms_userrole/Controller/UserRoleController.java
@@ -4,35 +4,37 @@ import com.cibertec.ms_userrole.Model.UserRole;
 import com.cibertec.ms_userrole.Service.UserRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class UserRoleController {
 
-    @Autowired
-    private UserRoleService userRoleService;
+   @Autowired
+   private UserRoleService userRoleService;
 
-    @GetMapping(path = "/customer")
-    public ResponseEntity<Object> retriveAll() {
-        return userRoleService.getAll();
-    }
+   @GetMapping(path = "/userrole")
+   public ResponseEntity<Object> retriveAll() {
+      return userRoleService.getAll();
+   }
 
-    @GetMapping(path = "/customer/{user_id}")
-    public ResponseEntity<Object> retriveOne(@PathVariable Integer user_id) {
-        return userRoleService.getById(user_id);
-    }
+   @GetMapping(path = "/userrole/{user_id}")
+   public ResponseEntity<Object> retriveOne(@PathVariable Integer user_id) {
+      return userRoleService.getById(user_id);
+   }
 
-    @PostMapping(path = "/customer")
-    public ResponseEntity<Object> create(@RequestBody UserRole userRole) {
-        return userRoleService.insert(userRole);
-    }
+   @PostMapping(path = "/userrole")
+   public ResponseEntity<Object> create(@RequestBody UserRole userRole) {
+      return userRoleService.insert(userRole);
+   }
 
-    @PutMapping(path = "/customer/{id}")
-    public ResponseEntity<Object> update(@PathVariable Integer user_id, @RequestBody UserRole userRole) {
-        return userRoleService.update(user_id, userRole);
-    }
+   @PutMapping(path = "/userrole/{id}")
+   public ResponseEntity<Object> update(@PathVariable Integer user_id, @RequestBody UserRole userRole) {
+      return userRoleService.update(user_id, userRole);
+   }
 
-    @DeleteMapping(path = "/customer/{id}")
-    public ResponseEntity<Object> delete(@PathVariable Integer user_id) {
-        return userRoleService.delete(user_id);
-    }
+   @DeleteMapping(path = "/userrole/{id}")
+   public ResponseEntity<Object> delete(@PathVariable Integer user_id) {
+      return userRoleService.delete(user_id);
+   }
 }
--- src/main/java/com/cibertec/ms_userrole/Model/UserRole.java
+++ src/main/java/com/cibertec/ms_userrole/Model/UserRole.java
@@ -1,19 +1,20 @@
 package com.cibertec.ms_userrole.Model;
 
 import lombok.*;
+
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Data
 @Table(name = "userrole")
 public class UserRole {
 
-    @Id
-    @Column(name = "num_role_id")
-    private Integer num_role_id;
+   @Column(name = "num_role_id", nullable = false)
+   private Integer num_role_id;
 
-    @Id
-    @Column(name = "num_user_id" )
-    private Integer num_user_id;
+   @Id
+   @Column(name = "num_user_id")
+   private Integer num_user_id;
 
 }
